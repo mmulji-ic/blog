f3cf0376631a121b96ada94ea553c944d67e1cfb
jrbgit <joanas@gmail.com>
2022-12-16 13:01:42 -0500

    Cosmos SDK: stargate - v0.45.4

---
+++ ts-client/mmuljiic.blog.blog/registry.ts
@@ -0,0 +1,9 @@
+import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCreatePost } from "./types/blog/tx";
+
+const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/mmuljiic.blog.blog.MsgCreatePost", MsgCreatePost],
+    
+];
+
+export { msgTypes }
